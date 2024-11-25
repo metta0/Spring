7b34499aa162f68a7d7c133aa2d131cbbd18c8fc
metta0 <jiterin908@gmail.com>
2024-11-25 12:53:52 +0900

    7. 스프링 DB접근기술 - JPA

--- IntroSpring/hellospring/hellospring/src/main/java/hello/hello_spring/SpringConfig.java
+++ IntroSpring/hellospring/hellospring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,9 +1,12 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +18,12 @@ import javax.swing.*;
 public class SpringConfig {
 
     DataSource dataSource;
+    EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
-
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -29,6 +33,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
