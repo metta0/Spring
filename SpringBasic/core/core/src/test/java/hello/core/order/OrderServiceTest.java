f804cb8f5f4b3b8c0ea99283bbeaa834b7a28dcc
metta0 <jiterin908@gmail.com>
2024-12-04 16:16:22 +0900

    Order, Discount finished

---
+++ SpringBasic/core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        Long memberId = 1L;
+
+        //when
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //then
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+    }
+}
