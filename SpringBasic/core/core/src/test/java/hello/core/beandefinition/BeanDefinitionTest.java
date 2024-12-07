128cad31fe34bd1e2e366207dc1bd97910ac9b55
metta0 <jiterin908@gmail.com>
2024-12-07 17:14:56 +0900

    5.스프링 컨테이너와 스프링 빈

---
+++ SpringBasic/core/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean(){
+        String[] beanDefinitinoNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitinoName : beanDefinitinoNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitinoName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " +beanDefinitinoName + "beanDefinitnion = " + beanDefinition);
+            }
+
+
+
+        }
+    }
+}
