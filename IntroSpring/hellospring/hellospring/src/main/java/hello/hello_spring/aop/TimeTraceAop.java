99a8de95594744fdbd00a95a576012c5b9f2df24
metta0 <jiterin908@gmail.com>
2025-04-22 16:31:33 +0900

    initail commit: servlet

--- IntroSpring/hellospring/hellospring/src/main/java/hello/hello_spring/aop/TimeTraceAop.java
+++ IntroSpring/hellospring/hellospring/src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -12,6 +12,7 @@ public class TimeTraceAop {
     @Around("execution(* hello.hello_spring..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
+        
         System.out.println("START: " + joinPoint.toString());
         try{
             return joinPoint.proceed();
